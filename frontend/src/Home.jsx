f29a39cd18da3a072b60ca6435e352a6fc2b87f6
raihannurhidayat <grandrx041@gmail.com>
2024-01-16 17:14:40 +0700

    add image

--- frontend/src/Home.jsx
+++ frontend/src/Home.jsx
@@ -3,12 +3,47 @@ import { useGetProductsQuery } from "./redux/api/productApiSlice";
 import Loader from "./components/Loader";
 import Header from "./components/Header";
 import Message from "./components/Message";
+import Product from "./pages/Products/Product";
 
 const Home = () => {
   const { keyword } = useParams();
-  const { data, isLoading, isError } = useGetProductsQuery();
-  
-  return <>{!keyword ? <Header /> : null}</>;
+  const { data, isLoading, isError } = useGetProductsQuery({ keyword });
+  return (
+    <>
+      {!keyword ? <Header /> : null}
+
+      {isLoading ? (
+        <Loader />
+      ) : isError ? (
+        <Message variant="error">
+          {isError?.data.message || isError.message}
+        </Message>
+      ) : (
+        <>
+          <div className="flex justify-between items-center">
+            <h1 className="ml-[20rem] mt-[10rem] text-[3rem]">
+              Special Products
+            </h1>
+            <Link
+              to="/shop"
+              className="bg-pink-600 text-black font-bold rounded-full py-2 px-10 mr-[18rem] mt-[10rem]"
+            >
+              Shop
+            </Link>
+          </div>
+          <div className="">
+            <div className="flex justify-center flex-wrap mt-[2rem]">
+              {data.products.map((product) => (
+                <div className="" key={product._id}>
+                  <Product product={product} />
+                </div>
+              ))}
+            </div>
+          </div>
+        </>
+      )}
+    </>
+  );
 };
 
 export default Home;
--- frontend/src/components/Message.jsx
+++ frontend/src/components/Message.jsx
@@ -4,13 +4,13 @@ const Message = ({ variant, children }) => {
       case "success":
         return "bg-green-100 text-green-800";
       case "error":
-        return "bg-red-100 text-red-800";
+        return "bg-red-100 text-black";
       default:
         return "bg-blue-100 text-blue-800";
     }
   };
 
-  return <div className={`p-4 rounded ${getVariant()}`}>{children}</div>;
+  return <div className={`p-4 rounded  ${getVariant()}`}>{children}</div>;
 };
 
 export default Message;
